240db04386618057ccf9f94fbbc5f93672dabe85
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-02-21 18:34:01 +0000

    guardando cambios

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -55,10 +55,39 @@ const getState = ({ getStore, getActions, setStore }) => {
 				//reset the global store
 				setStore({ demo: demo });
 			},
+
 			/* Aca va mi codigo */
-			handleSbmit: () => {
+			handleSbmitRegister: e  => {
 				e.preventdDefault()
-			},
+				const { name, email, password, active, apiURL } = getStore()
+
+				const { getFetch } = getActions()
+                const url = `${apiURL}/api/register`
+                const raw = JSON.stringify({
+                    name, email, password, active
+                })
+				const options = {
+                    method: 'POST',
+                    body: raw,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                const request = getFetch(url, options)
+                request.then((response) => response.json()).then((datos) => {
+                    console.log(datos)
+                    toast.success(datos.success)
+                    setStore({
+                        name: '',
+                        email: '',
+                        password: '',
+                        password_confirm: '',
+                        active: true,
+                    })
+                }).catch(error => console.log(error))
+
+            },
+
 			handleChange: e => {
 				const { name, value } = e.target
 				setStore({
