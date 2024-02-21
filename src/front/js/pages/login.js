bf30fafb236e807dae6cd0c792ca00a0813f827c
NickolaiGaming <nickocamel@gmail.com>
2024-02-21 13:31:09 -0500

    login listo falta register

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -7,8 +7,18 @@ export const Login = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Prueba </h1>
-		</div>
+		<>
+			<form onSubmit={actions.handleSubmitLogin} className='w-50 mx-auto py-5 my-5'>
+				<div className="form-group mb-3">
+					<label htmlFor="email" className="form-label">Email</label>
+					<input className='form-control' type="email" name="email" id="email" placeholder='Insert Email' value={store.email} onChange={actions.handleChange} />
+				</div>
+				<div className="form-group mb-3">
+					<label htmlFor="password" className="form-label">Password</label>
+					<input className='form-control' type="password" name="password" id="password" placeholder='Insert password' value={store.password} onChange={actions.handleChange} />
+				</div>
+				<button className="btn btn-primary btn-sm w-100">Login</button>
+			</form>
+		</>
 	);
 };
