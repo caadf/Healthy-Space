0f7b0568f33908ac8481a6a88cd54787fcc7abbc
NickolaiGaming <nickocamel@gmail.com>
2024-02-24 11:52:18 -0500

    busqueda de recetas con front terminado

--- src/front/js/component/listadecomida.js
+++ src/front/js/component/listadecomida.js
@@ -6,7 +6,7 @@ const ComidaList = ({ recetaData }) => {
 
     return (
         <>
-            <div className="meals d-flex">
+            <div className="meals d-flex m-3">
                 {recetaData.map((receta) => {
                     return <Comida key={receta.id} receta={receta} />;
 
