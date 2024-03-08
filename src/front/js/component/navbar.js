3276294c5eb9deeecf88978e0a1a5aee36d4b50f
NickolaiGaming <nickocamel@gmail.com>
2024-03-08 12:25:53 -0500

    solucionando front end con logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import hsImageUrl from "../../img/healthy-space.png";
 
 
 
@@ -10,7 +11,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg text-light bg-info bg-opacity-25">
 			<div className="container-fluid text-light">
-				<Link className="navbar-brand text-info fs-3" to="/">Healthy Space</Link>
+				<Link className="navbar-brand text-primary" to="/"><img src={hsImageUrl} className=" " alt="..." style={{ "width": "10%" }} /></Link>
 				{
 					!!store.user ? (
 						<>
@@ -20,9 +21,9 @@ export const Navbar = () => {
 							</button>
 							<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 								<div class="navbar-nav ms-auto">
-									<Link to="/busquedareceta" className="nav-link text-dark fs-5 btn btn-outline-success mx-1">Busqueda de recetas</Link>
-									<Link to="/calculoplanalimenticio" className="nav-dark text-dark fs-5 btn btn-outline-success mx-1">Calculo Plan alimenticio</Link>
-									<Link to="/gastocalorico" className="nav-link text-dark fs-5 btn btn-outline-success mx-1">Calculo Gasto Calorico</Link>
+									<Link to="/busquedareceta" className="nav-link text-dark fs-3 btn btn-outline-success mx-1">Busqueda de recetas</Link>
+									<Link to="/calculoplanalimenticio" className="nav-dark text-dark fs-3 btn btn-outline-success mx-1">Calculo Plan alimenticio</Link>
+									<Link to="/gastocalorico" className="nav-link text-dark fs-3 btn btn-outline-success mx-1">Calculo Gasto Calorico</Link>
 									<p className="nav-link text-dark fs-5">{store.user.name}</p>
 									<button className="nav-link bg-secondary text-light rounded-3" onClick={actions.logout}>Logout</button>
 
@@ -40,8 +41,8 @@ export const Navbar = () => {
 							</button>
 							<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 								<div class="navbar-nav ms-auto">
-									<Link to="/login" className="nav-link text-dark fs-5 btn btn-outline-success mx-1">Login</Link>
-									<Link to="/register" className="nav-link text-dark fs-5 btn btn-outline-success">Register</Link>
+									<Link to="/login" className="nav-link text-dark fs-3 btn btn-outline-success mx-1">Login</Link>
+									<Link to="/register" className="nav-link text-dark fs-3 btn btn-outline-success">Register</Link>
 
 								</div>
 							</div>
